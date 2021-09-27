0c63a70d780786b0844ab1e8eab63c742e652786
Aqeem Abdullah <aqeemabd@gmail.com>
2021-09-27 09:23:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type='text'/>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Complete</button>
+      <button onClick={handleClearTodos}>Delete Complete</button>
       <div>{todos.filter(todo => todo.complete).length} left to do</div>
     </>
   );
